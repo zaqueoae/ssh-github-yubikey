53b738071be044bbfe70b5c119d989e1238af430
Zaqueo Echeverria <zaqueoae@gmail.com>
2023-09-26 13:03:43 +0200

    Update git-ssh.sh

--- git-ssh.sh
+++ git-ssh.sh
@@ -114,27 +114,26 @@ fi
 PATHKEY=~/.ssh/id_rsagithub
 if [ -f ~/.ssh/id_rsagithub ]; then
     SUFIX=$(date +"%T.%N" | md5sum | base64 | head -c 3)
-    PATHKEY="~/.ssh/id_rsagithub$SUFIX"
 fi
 
 if [ "$KEYS" = 1 ]; then
-    ssh-keygen -b 4096 -t rsa -f "$PATHKEY" -q -N ""
+    ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsagithub"$SUFIX" -q -N ""
 fi
 if [ "$USB" = 1 ]; then
-    ssh-keygen -t ed25519-sk -O resident -O no-touch-required -O application=ssh:id_rsagithub$SUFIX -f $PATHKEY -P ""
+    ssh-keygen -t ed25519-sk -O resident -O no-touch-required -O application=ssh:id_rsagithub$SUFIX -f ~/.ssh/id_rsagithub$SUFIX -P ""
 fi
 if [ "$USB" = 2 ]; then
-    ssh-keygen -t ed25519-sk -O resident -O verify-required -O no-touch-required -O application=ssh:id_rsagithub$SUFIX -f $PATHKEY -P ""
+    ssh-keygen -t ed25519-sk -O resident -O verify-required -O no-touch-required -O application=ssh:id_rsagithub$SUFIX -f ~/.ssh/id_rsagithub$SUFIX -P ""
 fi
 if [ "$USB" = 3 ]; then
-    ssh-keygen -t ed25519-sk -O resident -O application=ssh:id_rsagithub$SUFIX -f $PATHKEY -P ""
+    ssh-keygen -t ed25519-sk -O resident -O application=ssh:id_rsagithub$SUFIX -f ~/.ssh/id_rsagithub$SUFIX -P ""
 fi
 if [ "$USB" = 4 ]; then
-    ssh-keygen -t ed25519-sk -O resident -O verify-required -O application=ssh:id_rsagithub$SUFIX -f $PATHKEY -P ""
+    ssh-keygen -t ed25519-sk -O resident -O verify-required -O application=ssh:id_rsagithub$SUFIX -f ~/.ssh/id_rsagithub$SUFIX -P ""
 fi
 
-chmod 400 "$PATHKEY"
-chmod 644 "$PATHKEY".pub
+chmod 400 ~/.ssh/id_rsagithub"$SUFIX"
+chmod 644 ~/.ssh/id_rsagithub"$SUFIX".pub
 
 if ! (grep -wq "Include github" ~/.ssh/config); then
     sed  -i '1i Include github' ~/.ssh/config
@@ -144,12 +143,12 @@ rm -f ~/.ssh/github
 echo 'Host githubssh' >> ~/.ssh/github
 echo '        User git' >> ~/.ssh/github
 echo '        HostName github.com' >> ~/.ssh/github
-echo "        IdentityFile  $PATHKEY" >> ~/.ssh/github
+echo "        IdentityFile  ~/.ssh/id_rsagithub$SUFIX" >> ~/.ssh/github
 chmod 644 .ssh/github
 
 eval "$(ssh-agent)"
-ssh-add "$PATHKEY"
-pub=$(cat "$PATHKEY".pub)
+ssh-add ~/.ssh/id_rsagithub"$SUFIX"
+pub=$(cat ~/.ssh/id_rsagithub"$SUFIX".pub)
 echo ''
 echo ''
 echo ''
